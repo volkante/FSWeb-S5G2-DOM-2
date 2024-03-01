6ca203ee74ac0bb0fc4123b541e95428d96c136b
volkantemelli <volkan.temelli@gmail.com>
2024-03-01 15:34:58 +0300

    son
    yeniden yapildi

--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ import './less/index.less'
 /*nav a daki linklerin mouse üzerine gelince rengi değişsin. Mouse gidince
 eski renkler geri dönsün*/
 
-
 const navLinks = document.querySelectorAll(".nav-link");
 navLinks.forEach((item)=> {
     item.addEventListener("mouseover", ()=>{
@@ -23,7 +22,7 @@ navLinks.forEach((item)=> {
         item.style.backgroundColor ="";
         item.style.color ="";
     })
-    /*nav a daki linklere basınca linkler küçülsün. sonra eski haline dönsün */
+/*nav a daki linklere basınca linkler küçülsün. sonra eski haline dönsün */
     item.addEventListener("mousedown", () => {
         item.style.backgroundColor = "#d77a07";
         item.style.color = "white"
@@ -59,3 +58,67 @@ buttonsAboveFooter.forEach((item, index)=>{
     item.style.transform = `scale(${btnSizes[index]})`;
     });
 });
+
+//tum img'lere border olsun mouse üzerine gelince. sonra geri dönsün. renkleri uyumlu olsun.
+
+const imgColors = ["#feba4d", "#d5d3cf", "#7fbec0", "#9fcea2"];
+
+let amusementImgs = document.querySelectorAll("img");
+     
+amusementImgs.forEach((item,index)=>{
+    item.addEventListener("mouseover", (e)=>{
+        e.target.style.border = `solid 2px ${imgColors[index]}`;
+    });
+    item.addEventListener("mouseout", (e)=>{
+        e.target.style.border = "none";
+    });
+});
+
+// ilk otobus img'si sayfada scroll edince ters dönsün.
+
+const busImg = document.querySelector("img");
+
+document.addEventListener("wheel", ()=>{
+    busImg.style.transform = "rotate(180deg)";
+})
+
+
+
+
+//aynısı sonraki çalışmam
+// const navLinks = document.querySelectorAll(".nav-link")
+
+// navLinks.forEach(a=>{
+//     a.addEventListener("mouseover", changeColor);
+//     a.addEventListener("mouseout", changeBack)
+// })
+
+// function changeColor (event) {
+//     event.target.style.backgroundColor = "black";
+//     event.target.style.color = "white";
+// }
+
+// function changeBack (event) {
+//     event.target.style.backgroundColor = "";
+//     event.target.style.color = "";
+// }
+
+// //
+
+// navLinks.forEach(element=>{
+//     element.addEventListener("mousedown", ()=>{
+//         element.style.transition = "0s";
+//         element.style.transform = "scale(.9)";
+//     });
+//     element.addEventListener("mouseup", ()=>{
+//         element.style.transition = "0.3s";
+//         element.style.transform = "scale(1)"
+//     })
+// })
+
+// navLinks.forEach((item)=> {
+//     item.style.padding = "1rem";
+//     item.style.transition = ".2s";
+//     item.style.borderRadius = "0.5rem";
+// })
+
